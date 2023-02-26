9c4a28ab7af71374cc7b317f4e4b8222e137a496
CaedenPH <caedenperelliharris@gmail.com>
2023-02-26 21:31:37 +0000

    feat: Add base files

---
+++ src/game.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+
+# fmt: off
+__all__ = (
+    'Game'
+)
+# fmt: on
+
+class Game:
+    """
+    """
+    
+    def __init__(self) -> None:
+        ...
+
+    @classmethod
+    def play(cls) -> Game:
+        return cls()
