f3fb0070deaed8c899d14a9e0b47cf6dcef2b89a
CaedenPH <caedenperelliharris@gmail.com>
2023-02-26 21:35:22 +0000

    chore&doc: Fix tests and document Game class

--- src/game.py
+++ src/game.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-
 # fmt: off
 __all__ = (
     'Game'
 )
 # fmt: on
 
+
 class Game:
     """
+    Represents the game object which will be used to hold information
+    including the current game status, the current current board status
+    and other useful information about the game.
     """
-    
+
     def __init__(self) -> None:
         ...
 
